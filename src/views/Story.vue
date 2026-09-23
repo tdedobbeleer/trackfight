fde901867c9dd2a89edb84aa2b4bfcacfd6d5397
tdedobbeleer <tdedobbeleer@gmail.com>
2026-09-23 19:28:50 +0200

    add mention of provincial park from the start

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -13,7 +13,7 @@ const showVideo = ref(false)
   <BContainer class="py-5">
     <h1 class="fw-bold mb-4">{{ $t('story.title') }}</h1>
     <p class="lead story-text fw-bold">{{ $t('story.salutation') }}</p>
-    <p class="lead story-text">{{ $t('story.body1a') }}</p>
+    <p class="lead story-text" v-html="$t('story.body1a')"></p>
     <p class="lead story-text">{{ $t('story.body1b') }}</p>
     <p class="lead story-text">{{ $t('story.body1c') }}</p>
     <p class="lead story-text" v-html="$t('story.body1d')"></p>
